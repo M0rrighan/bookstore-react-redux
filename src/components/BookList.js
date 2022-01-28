5eb2a28126beeca8618ddbe700cfc95f7a0646a3
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-01-28 16:44:13 +0100

    Update BookList component: useEffect hook to dispatch loadListBooks from Api at page load

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { loadListBooks } from '../redux/books/booksMiddleware';
 import BookItem from './BookItem';
 
 const BookList = () => {
-  const booksList = useSelector((state) => state.booksReducer);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(loadListBooks());
+  }, []);
+
+  const booksList = useSelector((state) => state.booksReducer);
   return (
     <ul className="books-ul">
       {
